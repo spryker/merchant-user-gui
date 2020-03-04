c318bc6156844b8f84ccb3ec46030ee85e43c73a
pivovarov <stanislav.pivovarov@spryker.com>
2020-03-04 11:53:14 +0200

    add merchant user tab to the merchant edit page and table with users inside the tab

---
+++ src/Spryker/Zed/MerchantUserGui/Dependency/Facade/MerchantUserGuiToMerchantUserFacadeInterface.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace Spryker\Zed\MerchantUserGui\Dependency\Facade;
+
+use Generated\Shared\Transfer\MerchantUserCriteriaTransfer;
+use Generated\Shared\Transfer\MerchantUserResponseTransfer;
+use Generated\Shared\Transfer\MerchantUserTransfer;
+
+interface MerchantUserGuiToMerchantUserFacadeInterface
+{
+    /**
+     * @param \Generated\Shared\Transfer\MerchantUserTransfer $merchantUserTransfer
+     *
+     * @return \Generated\Shared\Transfer\MerchantUserResponseTransfer
+     */
+    public function create(MerchantUserTransfer $merchantUserTransfer): MerchantUserResponseTransfer;
+
+    /**
+     * @param \Generated\Shared\Transfer\MerchantUserTransfer $merchantUserTransfer
+     *
+     * @return \Generated\Shared\Transfer\MerchantUserResponseTransfer
+     */
+    public function update(MerchantUserTransfer $merchantUserTransfer): MerchantUserResponseTransfer;
+
+    /**
+     * @param \Generated\Shared\Transfer\MerchantUserCriteriaTransfer $merchantUserCriteriaTransfer
+     *
+     * @return \Generated\Shared\Transfer\MerchantUserTransfer|null
+     */
+    public function find(MerchantUserCriteriaTransfer $merchantUserCriteriaTransfer): ?MerchantUserTransfer;
+}
